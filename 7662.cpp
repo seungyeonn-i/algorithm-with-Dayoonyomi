e641ce3a8f5b213af20c355439c39a185ab96e01
seungyeonnnnnni <jeongsy8928@soongsil.ac.kr>
2021-07-21 18:56:34 +0900

    wae DDo na man error bombb

---
+++ 7662.cpp
@@ -0,0 +1,56 @@
+//D 1 최댓값 삭제
+//D -1 최솟값 삭제
+
+#include <stdio.h>
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main(){
+    int n,a;
+    scanf("%d",&n);
+    while(n--){
+
+        set<int>arr;
+        scanf("%d",&a);
+
+        for(int k=0;k<a;k++){
+
+            char i;
+            cin >> i;
+            int j;
+            cin >> j;
+
+            if(i == 'I')
+                arr.insert(j);
+            else{
+                
+                    if(!arr.empty() && j == 1){ //max delete
+                        auto p = arr.end(); // 끝을 가리킨다
+                        p--;//??? 그 앞에꺼 
+                        // end는 마지막 원소의 다음을 가리킨다
+                        arr.erase(p);
+                    }
+                    else if(!arr.empty() && j == -1){
+                        auto p = arr.begin(); //auto ??
+                        // 초기화 값에 따라 알아서 데이터 타입을 정해주는 키워드 
+                        arr.erase(p);
+                    }
+                
+
+            }
+
+        }
+        if(arr.empty())
+            printf("EMPTY\n");
+        else{
+            auto end = arr.end();
+            end--;
+            // cout << *end << ' ' << *arr.begin() << '\n';
+            printf("%d %d\n",*end,*arr.begin());
+        }
+    }
+    return 0;
+
+}
